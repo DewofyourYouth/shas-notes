28f29ee564285ed24b62c0594a57e83bba5f2249
DewofyourYouth <jacobshore@gmail.com>
2020-10-12 13:08:54 +0300

    first commit

---
+++ flask_mongo/__init__.py
@@ -0,0 +1,29 @@
+import pymongo
+from flask import Flask
+
+app = Flask(__name__)
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+
+sn = myclient["shasnotes"]
+mesechtos = sn["meseches"]
+
+@app.route('/add_meseches/<string:name>/<int:pages>/<int:item_num>')
+def add_meseches(name, pages, item_num):
+    mesechtos.insert_one({"name": name, "pages": pages, "itemNum": item_num})
+    return f"{name} added!"
+
+@app.route("/show_mesechtos")
+def show_mesechtos():
+    ms = mesechtos.find({"$query": {}, "$orderby": {"itemNum": 1}})
+    s = ""
+    for doc in ms:
+        s += f"{doc['name']}, {doc['pages']}\n"
+    return s
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
