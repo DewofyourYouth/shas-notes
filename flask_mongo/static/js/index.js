d1f33492cdc86977673d1f8baa83f224bcbb2038
DewofyourYouth <jacobshore@gmail.com>
2020-10-19 01:16:13 +0300

    quokka debugging

--- flask_mongo/static/js/index.js
+++ flask_mongo/static/js/index.js
@@ -1,16 +1,16 @@
-const tabBtns = document.querySelectorAll('.btn-tab')
-const tabContent = document.querySelectorAll('.tab-content')
+const tabBtns = document.querySelectorAll('.btn-tab') //?
+const tabContent = document.querySelectorAll('.tab-content') //?
 const regex = /tab-\d/i;
 
 
-tabBtns.forEach(item => {
+tabBtns.forEach(item => { 
     item.addEventListener('click', event => {
         event.preventDefault()
         tabBtns.forEach(tabs => {tabs.classList.remove('active')})
         tabContent.forEach(content => content.classList.remove('isActive'))
 
         event.target.classList.add('active')    
-        const el = String(event.target.classList).match(regex)[0]
+        const el = String(event.target.classList).match(regex)[0] 
         document.getElementById(el).classList.add('isActive')
     })
 })
